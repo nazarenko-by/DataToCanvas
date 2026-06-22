9785f0d5df0f1def179dd30173a7b84d656c0b8f
nazarenko-by <bodean56@ukr.net>
2026-06-23 00:59:19 +0300

    header: add cn

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { cn } from "@/src/helper";
 import { MENU_ITEMS } from "@src/helper/const";
 import { LogoMark } from "@/src/assets/icons";
 const ThemeSwitcher = dynamic(() => import("@src/components/Header/ThemeSwitcher"), {
@@ -10,14 +11,18 @@ const ThemeSwitcher = dynamic(() => import("@src/components/Header/ThemeSwitcher
 
 const Header = () => {
 	return (
-		<header className="w-full flex gap-5 justify-between items-center sticky top-0 z-[9999] p-5 xl:px-10 bg-background/80 backdrop-blur-sm border-b border-border text-sm">
+		<header
+			className={cn(
+				"w-full flex gap-5 justify-between items-center sticky top-0 z-[9999] p-5 xl:px-10 bg-background/80 backdrop-blur-sm border-b border-border text-sm"
+			)}
+		>
 			<div className="logo flex gap-2.5 items-center">
 				<LogoMark width={26} height={26} className="text-surface bg-accent rounded-md" />
 				<div className="logo-text font-bold text-[16px]">DataToCanvas</div>
 			</div>
 			<div className="menu flex gap-5 items-center text-text-2">
 				{MENU_ITEMS.map((item) => (
-					<a key={item.href} href={item.href} className="hover:text-text active:text-accent">
+					<a key={item.href} href={item.href} className={cn("hover:text-text")}>
 						{item.label}
 					</a>
 				))}
